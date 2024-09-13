0c3bade9f7567c69bdd9bd20f0ddee6df02f73d1
Paul L. <13600558+dev-paulL@users.noreply.github.com>
2024-09-13 18:05:09 +0200

    started jsx structure and theme setup (colors, fonts, sizes)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="bg-yellow100 text-Black">
+        <h2>Create and schedule content <span className="text-purple500"><em>quicker</em>.</span></h2>
+        <img src="../src/assets/images/illustration-create-post.webp" alt="" />
+        {/* illustration create post */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      {/* MAIN DIV */}
+      <div className="bg-purple500 text-White">
+        <h1>
+          Social Media <span>10x</span> <em>Faster</em> with AI
+        </h1>
+
+        {/* Stars  */}
+        <p>Over 4,000 5-star reviews</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className="bg-purple100 text-Black">
+        <h2>Schedule to social media.</h2>
+          {/* Illustration schedule post */}
+        <p>Optimize post timings to publish content at the perfect time for your audience.</p>
+      </div>
+
+      <div className="bg-yellow500 text-Black">
+        <h2>Write your content using AI.</h2>
+        {/* illustration ai content  */}
+      </div>
+
+      <div className="bg-White text-Black">
+        {/* Illustration multiple account platforms */ }
+        <h2>Manage multiple accounts and platforms.</h2>
+      </div>
+
+      <div className="bg-yellow500 text-Black">
+        <h2>Maintain a consistent posting schedule.</h2>
+        {/* illustration consistent posting schedule */}
+      </div>
+
+      <div className="bg-White text-Black">
+        <h2 className="text-small font-normal"><span className="text-big font-medium">&#62;56%</span> <br/> faster audience growth</h2>
+        {/* illustration audience growth */}
+      </div>
+
+      <div className="bg-purple500 text-White">
+        {/* illustration grow followers */}
+        <h2>Grow followers with non-stop content.</h2>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple100: "hsl(254, 88%, 90%)",
+        purple500: "hsl(256, 67%, 59%)",
+        yellow100: "hsl(31, 66%, 93%)",
+        yellow500: "hsl(39, 100%, 71%)",
+        White: "hsl(0, 0%, 100%)",
+        Black: "hsl(0, 0%, 7%)",
+      },
+      fontSize: {
+        small: "1.125rem",
+        medium: "3rem",
+        big: "4rem",
+      },
+      fontFamily: {
+        sans: ['"DM Sans"'],
+      },
+      fontWeight: {
+        regular: 400,
+        medium: 500,
+      },
+    },
   },
   plugins: [],
 };
