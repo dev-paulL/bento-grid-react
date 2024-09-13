f179f80789b027ec70ea07cc13f95ba1268c58be
Paul L. <13600558+dev-paulL@users.noreply.github.com>
2024-09-13 20:34:06 +0200

    imported images. create columns

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
       <div className="bg-yellow100 text-Black">
         <h2>Create and schedule content <span className="text-purple500"><em>quicker</em>.</span></h2>
         <img src="../src/assets/images/illustration-create-post.webp" alt="" />
-        {/* illustration create post */}
       </div>
 
       {/* MAIN DIV */}
@@ -13,38 +12,38 @@ function App() {
           Social Media <span>10x</span> <em>Faster</em> with AI
         </h1>
 
-        {/* Stars  */}
+        <img src="../src/assets/images/illustration-five-stars.webp" alt="" />
         <p>Over 4,000 5-star reviews</p>
       </div>
 
       <div className="bg-purple100 text-Black">
         <h2>Schedule to social media.</h2>
-          {/* Illustration schedule post */}
+          <img src="../src/assets/images/illustration-schedule-posts.webp" alt="" />
         <p>Optimize post timings to publish content at the perfect time for your audience.</p>
       </div>
 
       <div className="bg-yellow500 text-Black">
         <h2>Write your content using AI.</h2>
-        {/* illustration ai content  */}
+        <img src="../src/assets/images/illustration-ai-content.webp" alt="" />
       </div>
 
       <div className="bg-White text-Black">
-        {/* Illustration multiple account platforms */ }
+        <img src="../src/assets/images/illustration-multiple-platforms.webp" alt="" />
         <h2>Manage multiple accounts and platforms.</h2>
       </div>
 
       <div className="bg-yellow500 text-Black">
         <h2>Maintain a consistent posting schedule.</h2>
-        {/* illustration consistent posting schedule */}
+        <img src="../src/assets/images/illustration-consistent-schedule.webp" alt="" />
       </div>
 
       <div className="bg-White text-Black">
         <h2 className="text-small font-normal"><span className="text-big font-medium">&#62;56%</span> <br/> faster audience growth</h2>
-        {/* illustration audience growth */}
+        <img src="../src/assets/images/illustration-audience-growth.webp" alt="" />
       </div>
 
       <div className="bg-purple500 text-White">
-        {/* illustration grow followers */}
+        <img src="../src/assets/images/illustration-grow-followers.webp" alt="" />
         <h2>Grow followers with non-stop content.</h2>
       </div>
     </main>
